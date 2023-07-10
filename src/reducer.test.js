76116767452458b70deacfe09850d1329e63f4a5
Zu-shi <accountistaken@gmail.com>
2023-07-10 14:28:19 -0400

    Pass exercise 6.1, added tests and complementation for counter reducers

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -17,17 +17,51 @@ describe('unicafe reducer', () => {
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
+  // EG, nothing is undefined.
 
-  test('good is incremented', () => {
+  test('all are incremented', () => {
     const action = {
       type: 'GOOD'
     }
     const state = initialState
 
     deepFreeze(state)
-    const newState = counterReducer(state, action)
+    let newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+
+    const action2 = {
+      type: 'OK'
+    }
+
+    const action3 = {
+      type: 'BAD'
+    }
+
+    const action4 = {
+      type: 'ZERO'
+    }
+
+    newState = counterReducer(newState, action2)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 1,
+      bad: 0
+    })
+
+    newState = counterReducer(newState, action3)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 1,
+      bad: 1
+    })
+
+    newState = counterReducer(newState, action4)
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
